0202edbc5ca8ca0aba9f1ffb201011c519974ff2
MMostella <masonmostella@gmail.com>
2021-08-17 15:57:45 -0500

    Done with form. Adding styling

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,49 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
 
+import styled from 'styled-components';
 import './App.css';
 
-const initialFormValues = {
-  Name: '',
-  Email: '',
-  Role: ''
-}
+// const styled = 
+
+const membersList = [
+  { Name: 'Mason', Email: 'masonmostella@gmail.com', Role: 'Student' }
+]
 
 export default function App() {
-  // const [teamMembers, setTeamMembers] = useState([]);
-  const [formValues, setFormValues] = useState(initialFormValues);
-  const [error, setError] = useState();
-  // console.log(formValues);
+  const [formValues, setFormValues] = useState({Name: '', Email: '', Role: ''});
+  const [members, setMembers] = useState(membersList);
 
-  const updateForm = (inputName, inputValue) => {
-    setFormValues({...formValues, [inputName]: inputValue});
+  const change = (event) => {
+    const { name, value } = event.target;
+
+    setFormValues({...formValues, [name]: value })
   }
 
-  const submitForm = () => {
+  const submit = (event) => {
+    event.preventDefault();
     const newMember = {
-      name: formValues.name.trim(),
-      email: formValues.email.trim(),
-      role: formValues.role
-    };
-
-    if (!newMember.name || !newMember.email || !newMember.role) {
-      setError('Make Sure To Fill Out Every Field');
-      return;
+      Name: formValues.Name,
+      Email: formValues.Email,
+      Role: formValues.Role
     }
-
-    
+    setMembers(members.concat(newMember));
+    setFormValues({Name: '', Email: '', Role: ''})
   }
-  
+
   return (
-    <div>
-      <h1>Testing</h1>
-      <h3 className='error'>{error}</h3>
+    <>
       <Form 
-        values={formValues}
-        update={updateForm}
-        submit={submitForm}
+      values={formValues}
+      change={change}
+      submit={submit}
+      members={members}
       />
-    </div>
+    </>
   )
   
 }
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,59 +1,55 @@
 import React from 'react';
 
 export default function Form(props) {
-    const { values, update, submit } = props;
+    const { values, change, members, submit } = props;
 
-    const onChange = evt => {
-        // console.log(evt.target.name);
-        // console.log(evt.target.value);
-        const name = evt.target.name;
-        const value = evt.target.value;
-        update(name, value);
-    }
-
-    const onSubmit = evt => {
-        evt.preventDefault();
-        submit();
-    }
-
-    return (    
-        <form className='form container' onSubmit={onSubmit}>
-            <div className='form-group inputs'>
-                <label>Name
-                    <input 
-                        type='text'
-                        name='name'
-                        value={values.name}
-                        onChange={onChange}
-                        placeholder='Type Name'
-                    />
-                </label>
-                <label>Email
-                    <input 
-                        type='email'
-                        name='email'
-                        value={values.email}
-                        onChange={onChange}
-                        placeholder='Type Email'
-                    />
-                </label>
-                <label>Role
-                    <select 
-                        name='role'
-                        value={values.role}
-                        onChange={onChange}
-                    >
-                        <option value=''>-- Select a Role --</option>
-                        <option value='Student'>Student</option>
-                        <option value='TL'>Team Lead</option>
-                        <option value='Instructor'>Instructor</option>
-                        <option value='Alumni'>Alumni</option>
-                    </select>
-                </label>
-                <div className='submit' >
-                    <button>SUBMIT</button>
-                </div>
+    return (
+        <div>
+          <h1>Member Form App</h1>
+          {members.map((member, index) => (
+            <div key={index}>
+              {member.Name} is a {member.Role}. Contact them at {member.Email}
             </div>
-        </form>
-    )
+          ))}
+          <div>-----------------------------</div>
+          <form className='form container' onSubmit={submit}>
+            <div className='form-group inputs'>
+              <label>Name
+                <input 
+                  type='text'
+                  name='Name'
+                  value={values.Name}
+                  onChange={change}
+                  placeholder='Type Name'
+                />
+              </label>
+              <label>Email
+                <input 
+                  type='email'
+                  name='Email'
+                  value={values.Email}
+                  onChange={change}
+                  placeholder='Type Email'
+                />
+              </label>
+              <label>Role
+                <select 
+                  name='Role'
+                  value={values.Role}
+                  onChange={change}
+                >
+                  <option value=''>-- Select a Role --</option>
+                  <option value='Student'>Student</option>
+                  <option value='TL'>Team Lead</option>
+                  <option value='Instructor'>Instructor</option>
+                  <option value='Alumni'>Alumni</option>
+                </select>
+              </label>
+                <div className='submit' >
+                  <button>ADD MEMBER</button>
+                </div>
+                </div>
+            </form>
+          </div>
+      )
 }
